dcd9b3ee7678d4eeb00cb5f082e53baa4fc25389
debashish <debashishdash03412@gmail.com>
2024-10-26 14:14:06 +0530

    added spotlight movingborder configured tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.06]">
-    <h1 className="text-3xl text-center">Hello</h1>  
+    <h1 className="text-3xl text-center">Hello</h1> 
+    <HeroSection />
     </main>
   );
 }
